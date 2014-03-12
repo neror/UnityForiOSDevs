09a124f56f68958fe73dedef3738f7c64bc2ed2d
Nathan Eror <neror@neror.com>
2014-03-12 16:34:50 -0500

    Add PlaceOnObjectBelow example

---
+++ CubesAndStuff/Assets/Scripts/MartianCraft/Common/MCBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MartianCraft.Common
+{
+	public class MCBehaviour : MonoBehaviour
+	{
+		#region Cached Component Properties
+
+		private Transform mCachedTransform;
+		public Transform cachedTransform
+		{
+			get
+			{
+				if(mCachedTransform == null) {
+					mCachedTransform = GetComponent<Transform>();
+				}
+				return mCachedTransform;
+			}
+		}
+		
+		private Camera mCachedMainCamera;
+		public Camera cachedMainCamera
+		{
+			get
+			{
+				if(mCachedMainCamera == null) {
+					mCachedMainCamera = Camera.main;
+				}
+				return mCachedMainCamera;
+			}
+		}
+		
+		private Rigidbody mCachedRigidbody;
+		public Rigidbody cachedRigidbody
+		{
+			get
+			{
+				if(mCachedRigidbody == null) {
+					mCachedRigidbody = GetComponent<Rigidbody>();
+				}
+				return mCachedRigidbody;
+			}
+		}
+		
+		private Renderer mCachedRenderer;
+		public Renderer cachedRenderer
+		{
+			get
+			{
+				if(mCachedRenderer == null) {
+					mCachedRenderer = GetComponent<Renderer>();
+				}
+				return mCachedRenderer;
+			}
+		}
+
+		private Rigidbody2D mCachedRigidbody2D;
+		public Rigidbody2D cachedRigidbody2D
+		{
+			get
+			{
+				if(mCachedRigidbody2D == null) {
+					mCachedRigidbody2D = GetComponent<Rigidbody2D>();
+				}
+				return mCachedRigidbody2D;
+			}
+		}
+
+		private Collider mCachedCollider;
+		public Collider cachedCollider
+		{
+			get
+			{
+				if(mCachedCollider == null) {
+					mCachedCollider = GetComponent<Collider>();
+				}
+				return mCachedCollider;
+			}
+		}
+		
+		#endregion
+	}
+
+}
---
+++ CubesAndStuff/Assets/Scripts/MartianCraft/Common/PlaceOnObjectBelow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MartianCraft.Common
+{
+	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent(typeof(Collider))]
+	public class PlaceOnObjectBelow : MCBehaviour
+	{
+		public LayerMask surfaceLayers = 1;  // LayerMask == 1 is the "Default" layer
+		public bool resetOnEnable = false;
+		private bool initialized = false;
+		private bool wasKinematic;
+
+		void OnEnable()
+		{
+			if(!resetOnEnable) {
+				wasKinematic = cachedRigidbody.isKinematic;
+				cachedRigidbody.isKinematic = true;
+				initialized = false;
+			}
+		}
+
+		void FixedUpdate()
+		{
+			if(!initialized) {
+				Initialize();
+				initialized = true;
+				cachedRigidbody.isKinematic = wasKinematic;
+			}
+		}
+
+		[ContextMenu("Place Now")]
+		void Initialize()
+		{
+			RaycastHit hit;
+			if(Physics.Raycast(cachedTransform.position, Vector3.down, out hit, float.MaxValue, surfaceLayers.value)) {
+
+				// Align the up vector to the surface normal
+				cachedTransform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+				// Place the rotated object on the surface
+				Vector3 pos = cachedTransform.position;
+				pos.y = hit.point.y + cachedCollider.bounds.extents.y;
+				cachedTransform.position = pos;
+			}
+		}
+	}
+}
